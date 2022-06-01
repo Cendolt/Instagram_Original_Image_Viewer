c283a6784aaa3e60c75d548335650ae6b673e999
Cendolt <kendoruto@gmail.com>
2022-06-01 15:49:03 +0900

    Updated button text appearance and algorithm. Updated some selectors to reflect Instagram class name changes.

--- insta_original_image_viewer_main.user.js
+++ insta_original_image_viewer_main.user.js
@@ -11,18 +11,33 @@
 // @grant		GM_addElement
 // ==/UserScript==
 
+var resizeObserver = new ResizeObserver(entries => {
+	entries.forEach (entry => {
+		var imgContainer = entry.target.classList.contains("_aa06") ? entry.target : null;
+		if (imgContainer) {
+			var btn = imgContainer.querySelector('[class="IMBTN"]');
+			if (btn) {
+				btn.style.setProperty("--BTN_bg_sizes", imgContainer.offsetWidth + "px");
+			}	
+		}
+	});
+});
+
 function updateImgContainers() {
-	const imgContainers = document.querySelectorAll('[class="ZyFrc"]');
+	resizeObserver.disconnect();
+	const imgContainers = document.querySelectorAll('[class="_aa06"]');
 	imgContainers.forEach( imgContainer =>
 	{
 		imgContainer.addEventListener ("mouseenter", onImageMouseEnter, false );
 		imgContainer.addEventListener ("mouseleave", onImageMouseLeave, false );
+
+		resizeObserver.observe(imgContainer);
 	});
 };
 
 function onImageMouseEnter(event) {
 	var imgContainer = event.target;
-	const img = imgContainer.querySelector('[class="FFVAD"]');
+	const img = imgContainer.querySelector('[class="_aagt"]');
 	if (img != null) {
 		var imgSrc = img.getAttribute('src');
 		addDLBtnToImage(imgContainer, imgSrc);
@@ -31,44 +46,60 @@ function onImageMouseEnter(event) {
 
 function onImageMouseLeave(event) {
 	var imgContainer = event.target;
-	const img = imgContainer.querySelector('[class="FFVAD"]');
+	const img = imgContainer.querySelector('[class="_aagt"]');
 	if (img != null) {
 		removeDLBtnFromImage(imgContainer);
 	}
 }
 
 /*
+* @description Add a "View original" button to the image 
 * @param {Element}	imgContainer	Image for which to add the button
 * @param {String}	imgSrc			Link to the original image file
 */
 function addDLBtnToImage(imgContainer, imgSrc) {
-	if (imgContainer.querySelector('[class="DUMMYBTN"]')) return;
-	var btnDiv = imgContainer.querySelector('[class="_9AhH0"]');
+	var btnDiv = imgContainer.querySelector('[class="_aagw"]');
 
-	var dummybtn = GM_addElement(btnDiv, 'div', {
-		class: 'DUMMYBTN'
-	});
-
-	var btn = GM_addElement(dummybtn, 'button', {
-		class: 'IMBTN',
-		type: 'button',
-		imgSrc: imgSrc,
-		textContent: 'View original'
-	});
-
-	btn.addEventListener ("click", onDlBtnClick, false);
+	var btn = imgContainer.querySelector('[class="IMBTN"]');
+	if (btn == null) {
+		btn = GM_addElement(btnDiv, 'button', {
+			class: 'IMBTN',
+			type: 'button',
+			textContent: 'View original'
+		});
+		btn.style.setProperty("--BTN_bg_sizes", imgContainer.offsetWidth + "px");
+		btn.addEventListener("click", onDlBtnClick, false);
+	}
+	if (imgSrc) {
+		btn.setAttribute("imgSrc", imgSrc);
+		btn.style["background-image"] = 'url("' + imgSrc +'")';
+	}
+	
+	var dummybtn = imgContainer.querySelector('[class="DUMMYBTN"]');
+	if (dummybtn == null) {
+		dummybtn = GM_addElement(btnDiv, 'div', {
+			class: 'DUMMYBTN',
+			tabindex: '-1',
+			textContent: 'View original'
+		});
+	}
 }
 
 /*
 * @param {Element}	imgContainer	Image to remove the button from
 */
 function removeDLBtnFromImage(imgContainer) {
+	var btnDiv = imgContainer.querySelector('[class="_aagw"]');
+	
+	var btn = imgContainer.querySelector('[class="IMBTN"]');
+	if (btn) {
+		btn.removeEventListener("click", onDlBtnClick, false);
+		btnDiv.removeChild(btn);
+	}
 	var dummybtn = imgContainer.querySelector('[class="DUMMYBTN"]');
-	if (dummybtn == null) return;
-	imgContainer.querySelector('[class="IMBTN"]').removeEventListener (
-		"click", onDlBtnClick, false);
-	var btnDiv = imgContainer.querySelector('[class="_9AhH0"]');
-	btnDiv.removeChild(dummybtn);
+	if (dummybtn) {	
+		btnDiv.removeChild(dummybtn);
+	}
 }
 
 /*
@@ -88,53 +119,65 @@ function onDocumentMutation(changes, observer) {
 
 window.addEventListener('load', function(){
 	GM_addStyle(`
-    .DUMMYBTN{
+	._aagw .DUMMYBTN, ._aagw .IMBTN{
+		--BTN_margin: 4px;
+		--BTN_padding: 3px 8px;
+		--BTN_radius: 25px;
+
 		position: absolute;
 		right: 0;
 		bottom: 0;
-		margin: 3px;
 		width:fit-content;
-		padding: -1px;
-		background: rgba(255,255,255,0.8);
-		border-radius: 25px;
-		border-width: 1px;
-		border-color: rgba(255,255,255,0.0);
-		background-clip:padding-box;
-		mix-blend-mode: normal;
+		font-weight: var(--font-weight-system-semibold);
+		text-align: center;
+
+		margin: var(--BTN_margin);
+		padding: var(--BTN_padding);
+		border-radius: var(--BTN_radius);
 	}
 
-	.DUMMYBTN .IMBTN{
+    ._aagw .DUMMYBTN{
+		color: transparent;
+		background: rgba(255,255,255,.8);
+		box-shadow: 0px 0px 3px rgba(38,38,38,1);
+
+		z-index: 3;
+	}
+
+	._aagw .IMBTN{
+		--BTN_bg_sizes: 100%;
 		cursor: pointer;
-		//width:fit-content;
-		padding: 3px 8px;
-		margin: -1px;
-		font-weight: 600;
-		background: transparent;
-		border: 1px solid rgba(255,255,255,0.0);
-		border-radius: 25px;
-		background-clip:border-box;
-	  	mix-blend-mode: color-burn;
+
+		border: 0px solid rgba(255,255,255,0.0);
+
+		background-position: bottom calc(0px - var(--BTN_margin))
+							 right calc(0px - var(--BTN_margin));
+		background-size: var(--BTN_bg_sizes) auto;
+		background-repeat: no-repeat;
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: rgba(38,38,38,0.25);
+
+		z-index: 4;
 	}
 
-    .DUMMYBTN:hover{
-		background: rgba(255,255,255,0.8);
+    ._aagw .IMBTN:hover ~ .DUMMYBTN{
+		--BTN_margin: 3px;
+		--BTN_padding: 4px 9px;
 	}
 
-	.DUMMYBTN:hover .IMBTN{
-	  	mix-blend-mode: normal;
+	._aagw .IMBTN:hover{
+		
 	}
 
- 	.DUMMYBTN:active{
+	._aagw .IMBTN:active ~ .DUMMYBTN{
 		background: rgba(255,255,255,0.9);
-		padding: 0px;
-		background-clip:padding-box;
+		--BTN_margin: 4px;
+		--BTN_padding: 3px 8px;
 	}
 
-	.IMBTN:active{
-		color: rgba(142,142,142,1);
-		border-color: rgba(142,142,142,1);
-		background-clip:margin-box;
-	  	mix-blend-mode: normal;
+	._aagw .IMBTN:active{
+		-webkit-text-fill-color: rgba(38,38,38,0.75);
 	}
 
     `);
